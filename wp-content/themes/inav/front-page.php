96461a5fecab72ed510e4ac818825c82617b30cb
Sinsl <sinsl@me.com>
2023-11-12 23:41:10 +0300

    change 10.11

--- wp-content/themes/inav/front-page.php
+++ wp-content/themes/inav/front-page.php
@@ -65,13 +65,99 @@ get_header();
                     <div class="sputnik-title">КА “глонасс-К2”</div>
                 </div>
             </div>
-        </div>
-
-		
+        </div>		
     </article>
     <article class="front-box-item box-products">
         <div id="round-top-right"></div>
+        <div class="content-box products-content">
+            <div class="products-container">
+                <a href="" class="product-item product-item_1">
+                    <div class="product-item_img product-img_1"></div>
+                    <div class="roduct-item_title">системные научные исследования</div>
+                </a>
+                <a href="" class="product-item product-item_2">
+                    <div class="product-item_img product-img_2"></div>
+                    <div class="roduct-item_title">сопровождение государственных программ</div>
+                </a>
+                <a href="" class="product-item product-item_3">
+                    <div class="product-item_img product-img_3"></div>
+                    <div class="roduct-item_title">средства сертификации глонасс</div>
+                </a>
+                <a href="" class="product-item product-item_4">
+                    <div class="product-item_img product-img_4"></div>
+                    <div class="roduct-item_title">сверхпоглощающие (сверхчерные) покрытия</div>
+                </a>
+                <div class="product-title">
+                    <h2>продукция и услуги</h2>
+                </div>
+                <a href="" class="product-item product-item_6">
+                    <div class="product-item_img product-img_6"></div>
+                    <div class="roduct-item_title">компоненты микроприборов</div>
+                </a>
+                <a href="" class="product-item product-item_7">
+                    <div class="product-item_img product-img_7"></div>
+                    <div class="roduct-item_title">приборы контроля условий нрс</div>
+                </a>
+                <a href="" class="product-item product-item_8">
+                    <div class="product-item_img product-img_8"></div>
+                    <div class="roduct-item_title">международное сотрудничество</div>
+                </a>
+                <a href="" class="product-item product-item_9">
+                    <div class="product-item_img product-img_9"></div>
+                    <div class="roduct-item_title">it деятельность</div>
+                </a>
+            </div>
+        </div>
     </article>
+    <article class="front-box-item box-glonass">
+        <div class="glonass-fon"></div>
+        <div class="content-box glonass-content">
+            <div class="glonass-title-box">
+                <div class="glonass-title">глонасс</div>
+                <div class="glonass-title-img"></div>
+            </div>
+            <div class="glonass-text">АО «Институт навигации» ведет разработку комплекса средств сертификации системы ГЛОНАСС</div>
+            <a class="btn-detailed btn-light" href="#">подробнее</a>
+        </div>        
+    </article>
+    <article class="front-box-item box-school">
+        <div id="round-bottom-left"></div>
+        <div class="content-box school-content">
+            <div class="school-img-box">
+                <div class="school-img_item school-img_1"></div>
+                <div class="school-img_item school-img_2"></div>
+                <div class="school-img_item school-img_3"></div>
+            </div>
+            <div class="school-text-box">
+                <div class="school-title">научная школа</div>
+                <div class="run-line">
+					<div class="run-point"></div>
+				</div>
+                <div class="school-text">В АО «Институт навигации» с момента основания сформирована, действует и развивается научная школа создания оптико-электронных и сверхвысокочастотных полупроводниковых приборов, модулей.</div>
+                <a class="btn-detailed" href="#">подробнее</a>
+            </div>
+        </div>
+    </article>
+    <article class="front-box-item box-career">
+        <div class="content-box career-content">
+            <div class="career-text-box">
+                <div class="career-text_title">карьера</div>
+                <div class="run-line">
+					<div class="run-point"></div>
+				</div>
+                <div class="career-text_txt">АО “Институт навигации” ищет специалистов по основным направлениям свой деятельности.</div>
+            </div>
+            <div class="career-btn-box">
+                <a class="btn-detailed btn-light" href="#">подробнее</a>
+            </div>
+            <div class="career-img-box">
+                <div class="career-img-box_fon">
+                    <div class="career-img-box_img"></div>
+                </div>
+            </div>
+        </div>
+    </article>
+    
 </div>
 
 <?php
